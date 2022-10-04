5ad31a3e0227bbb400735283b2851a5acd4ddb46
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-04 16:14:02 +0700

    add constants hepers

---
+++ lib/helpers/asset_helper.dart
@@ -0,0 +1,7 @@
+mixin AssetHelper {
+  //Assets in icons
+
+  //Assets in images
+  static const String backgroundSplash = 'assets/images/background_splash.png';
+  static const String circleSplash = 'assets/images/circle_splash.png';
+}
