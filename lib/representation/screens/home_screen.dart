a2638ea2969891beb95e44c086980d48a5ffbdd9
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-04 22:19:48 +0700

    complete appbar

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,3 +1,7 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../core/constants/dismension_constants.dart';
+import '../../core/constants/textstyle_constants.dart';
 import '../../representation/widgets/appbar_container.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +11,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBarContainerWidget(
-      titleString: 'Home',
-      implementLeading: true,
-      implementTraling: true,
-      child: Container(
-        color: Colors.amber,
+      // titleString: 'Home',
+      // implementLeading: true,
+      // implementTraling: true,
+      title: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kItemPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Xin chào!',
+                    style:
+                        TextStyles.defaultStyle.fontHeader.whiteTextColor.bold),
+                const SizedBox(
+                  height: kMediumPadding,
+                ),
+                Text(
+                  'Trải nghiệm ứng dụng để có những kit tuyệt vời',
+                  style: TextStyles.defaultStyle.fontCaption.whiteTextColor,
+                ),
+              ],
+            ),
+            const Spacer(),
+            const Icon(
+              FontAwesomeIcons.bell,
+              size: kDefaultIconSize,
+              color: Colors.white,
+            ),
+            const SizedBox(
+              width: kMinPadding,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  kItemPadding,
+                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.all(kItemPadding),
+              // child: ImageHelper.loadFromAsset(
+              //   AssetHelper.person,
+              // ),
+            ),
+          ],
+        ),
       ),
+      child: Container(),
     );
   }
 }
