f3ccc150bb7a216f4ae4cb0700b99b2f7d1cd65d
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-04 21:06:26 +0700

    add appbar background

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import '../../representation/widgets/appbar_container.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AppBarContainerWidget(
+      child: Container(
+        color: Colors.amber,
+      ),
+    );
   }
 }
