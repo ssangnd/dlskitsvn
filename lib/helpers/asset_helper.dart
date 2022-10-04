24ae693c56d8fb2915555bfc3e80d65d1755e6aa
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-04 23:52:44 +0700

    add route and ui home league

--- lib/helpers/asset_helper.dart
+++ lib/helpers/asset_helper.dart
@@ -3,6 +3,14 @@ mixin AssetHelper {
   static const String icoOvalTop = 'assets/icons/ico_oval_top.png';
   static const String icoOvalBottom = 'assets/icons/ico_oval_bottom.png';
 
+  //logo league
+  static const String icoVleague = 'assets/icons/ico_vleague.png';
+  static const String icoPpremierLeague = 'assets/icons/ico_premierleague.png';
+  static const String icoLaliga = 'assets/icons/ico_laliga.png';
+  static const String icoLigue1 = 'assets/icons/ico_ligue.png';
+  static const String icoBundesliga = 'assets/icons/ico_bundesliga.png';
+  static const String icoSeria = 'assets/icons/ico_seria.png';
+
   //Assets in images
   static const String backgroundSplash = 'assets/images/background_splash.png';
   static const String circleSplash = 'assets/images/circle_splash.png';
--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dlskitsvn/helpers/asset_helper.dart';
+import 'package:dlskitsvn/helpers/image_helper.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../core/constants/dismension_constants.dart';
@@ -5,8 +7,41 @@ import '../../core/constants/textstyle_constants.dart';
 import '../../representation/widgets/appbar_container.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildItemCategory(
+      Widget icon, Color color, Function() onTap, String title) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(
+              // vertical: kMediumPadding,
+              vertical: kMinPadding,
+            ),
+            decoration: BoxDecoration(
+              // color: color.withOpacity(0.8),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(kItemPadding),
+            ),
+            child: icon,
+          ),
+          const SizedBox(
+            height: kItemPadding,
+          ),
+          Text(title)
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +96,87 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      child: Container(),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(AssetHelper.icoVleague,
+                        width: 75, height: 75),
+                    const Color(0xffF77777),
+                    () {},
+                    'VLeague'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(AssetHelper.icoPpremierLeague,
+                        width: 75, height: 75),
+                    const Color(0xffF77777),
+                    () {},
+                    'Premier League'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(AssetHelper.icoLaliga,
+                        width: 75, height: 75),
+                    const Color(0xffF77777),
+                    () {},
+                    'Laliga'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: kMediumPadding,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(AssetHelper.icoLigue1,
+                        width: 75, height: 75),
+                    const Color(0xffF77777),
+                    () {},
+                    'Ligue 1'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(AssetHelper.icoBundesliga,
+                        width: 75, height: 75),
+                    const Color(0xffF77777),
+                    () {},
+                    'Bundesliga'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                child: _buildItemCategory(
+                    ImageHelper.loadFromAsset(AssetHelper.icoSeria,
+                        width: 75, height: 75),
+                    const Color(0xffF77777),
+                    () {},
+                    'Seria'),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
