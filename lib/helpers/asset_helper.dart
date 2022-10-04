edb377036b32231d75cc3ae401c2ae606b0a7c81
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-04 21:05:46 +0700

    add icon Oval

--- lib/helpers/asset_helper.dart
+++ lib/helpers/asset_helper.dart
@@ -1,5 +1,7 @@
 mixin AssetHelper {
   //Assets in icons
+  static const String icoOvalTop = 'assets/icons/ico_oval_top.png';
+  static const String icoOvalBottom = 'assets/icons/ico_oval_bottom.png';
 
   //Assets in images
   static const String backgroundSplash = 'assets/images/background_splash.png';
