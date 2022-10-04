bdd6bcece2c60ff71ba5d04a7d01fe0a0feccb30
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-04 17:08:39 +0700

    add bottom bar

--- lib/representation/screens/main_app.dart
+++ lib/representation/screens/main_app.dart
@@ -1,4 +1,9 @@
+import 'package:dlskitsvn/core/constants/color_constants.dart';
+import 'package:dlskitsvn/core/constants/dismension_constants.dart';
+import 'package:dlskitsvn/representation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -10,8 +15,45 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: IndexedStack(index: _currentIndex, children: [
+        const HomeScreen(),
+        Container(
+          color: Colors.red,
+        )
+      ]),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: ColorPalette.primaryColor.withOpacity(0.2),
+        unselectedItemColor: ColorPalette.primaryColor,
+        margin: const EdgeInsets.symmetric(
+            horizontal: kMediumPadding, vertical: kDefaultPadding),
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.house,
+              size: kDefaultIconSize,
+            ),
+            title: const Text('Home'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.guilded,
+              size: kDefaultIconSize,
+            ),
+            title: const Text('Guide'),
+          ),
+        ],
+      ),
+    );
   }
 }
