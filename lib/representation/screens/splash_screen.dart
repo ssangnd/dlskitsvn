369fe2585348e02b2536ad381da5927556abfaf3
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-04 16:14:30 +0700

    build splash screen

---
+++ lib/representation/screens/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../helpers/asset_helper.dart';
+import '../../helpers/image_helper.dart';
+import 'main_app.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  static String routeName = '/splash_screen';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _routeToIntroScreen();
+  }
+
+  void _routeToIntroScreen() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // ignore: use_build_context_synchronously
+    Navigator.of(context).pushNamed(MainApp.routeName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: ImageHelper.loadFromAsset(
+            AssetHelper.backgroundSplash,
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        Positioned.fill(
+          child: ImageHelper.loadFromAsset(
+            AssetHelper.circleSplash,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'representation/screens/main_app.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  MainApp.routeName: (context) => const MainApp(),
+};
