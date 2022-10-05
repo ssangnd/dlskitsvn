37f4e81370232512149d6bfe405b85c28b9cc23e
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-05 22:11:18 +0700

    add ItemModel

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+class ItemModel {
+  ItemModel({
+    required this.image,
+    required this.name,
+    required this.size,
+    required this.price,
+  });
+
+  final String image;
+  final String name;
+  final int size;
+  final int price;
+}
