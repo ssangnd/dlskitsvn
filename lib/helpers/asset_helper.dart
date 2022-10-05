98fffcd6ea8f7b016a620bfbdff608723ca0a630
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-05 17:25:45 +0700

    config asset icon

--- lib/helpers/asset_helper.dart
+++ lib/helpers/asset_helper.dart
@@ -10,6 +10,23 @@ mixin AssetHelper {
   static const String icoLigue1 = 'assets/icons/ico_ligue.png';
   static const String icoBundesliga = 'assets/icons/ico_bundesliga.png';
   static const String icoSeria = 'assets/icons/ico_seria.png';
+  //ico Vleague
+  static const String icoViettel = 'assets/icons/ico_vleague/ico_viettel.png';
+  static const String icoTphcm = 'assets/icons/ico_vleague/ico_tphcm.png';
+  static const String icoSlna = 'assets/icons/ico_vleague/ico_slna.png';
+  static const String icoThanhhoa = 'assets/icons/ico_vleague/ico_thanhhoa.png';
+  static const String icoNamdinh = 'assets/icons/ico_vleague/ico_namdinh.png';
+  static const String icoHatinh = 'assets/icons/ico_vleague/ico_hatinh.png';
+  static const String icoHanoi = 'assets/icons/ico_vleague/ico_hanoi.png';
+  static const String icoHaiphong = 'assets/icons/ico_vleague/ico_haiphong.png';
+  static const String icoHagl = 'assets/icons/ico_vleague/ico_hagl.png';
+
+  static const String icoDanang = 'assets/icons/ico_vleague/ico_danang.png';
+  static const String icoSaigon = 'assets/icons/ico_vleague/ico_saigon.png';
+  static const String icoBinhDuong =
+      'assets/icons/ico_vleague/ico_binhduong.png';
+
+  static const String icoBinhdinh = 'assets/icons/ico_vleague/ico_binhdinh.png';
 
   //Assets in images
   static const String backgroundSplash = 'assets/images/background_splash.png';
