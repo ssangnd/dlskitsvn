191cba737d157d446ad7c0f0d8d88778283bdac7
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-10-06 21:17:27 +0700

    add ligue team kit

---
+++ lib/representation/screens/ligue1s/monaco_screen.dart
@@ -0,0 +1,126 @@
+import '/representation/widgets/appbar_container.dart';
+import '/representation/widgets/item_kits_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import '../../../models/item_model.dart';
+import '../../../services/ad_service.dart';
+
+class MonacoScreen extends StatefulWidget {
+  const MonacoScreen({super.key});
+
+  static const String routeName = 'monaco_screen';
+
+  @override
+  State<MonacoScreen> createState() => _MonacoScreenState();
+}
+
+class _MonacoScreenState extends State<MonacoScreen> {
+  BannerAd? _banner;
+  @override
+  void initState() {
+    super.initState();
+    _createBannerAd();
+  }
+
+  void _createBannerAd() {
+    _banner = BannerAd(
+      size: AdSize.fullBanner,
+      adUnitId: AdService.bannerAdUnitId!,
+      listener: AdService.bannerAdListener,
+      request: const AdRequest(),
+    )..load();
+  }
+
+  final List<ItemModel> listRoom = [
+    ItemModel(
+      image: 'https://i.imgur.com/imMsp4b.png',
+      name: 'Logo',
+      size: 40,
+      price: 1,
+    ),
+    ItemModel(
+      image: 'https://i.imgur.com/WUw5v0t.png',
+      name: 'Home Kit',
+      size: 40,
+      price: 2,
+    ),
+    ItemModel(
+      image: 'https://i.imgur.com/qGnHw1Y.png',
+      name: 'Away Kit',
+      size: 40,
+      price: 3,
+    ),
+    ItemModel(
+      image: 'https://i.imgur.com/NMNCb6b.png',
+      name: 'Third Kit',
+      size: 40,
+      price: 3,
+    ),
+    ItemModel(
+      image: 'https://i.imgur.com/jB07q26.png',
+      name: 'Goalkeeper Home Kit',
+      size: 40,
+      price: 4,
+    ),
+    ItemModel(
+      image: 'https://i.imgur.com/pufnWqG.png',
+      name: 'Goalkeeper Away Kit',
+      size: 40,
+      price: 5,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return AppBarContainerWidget(
+      titleString: 'Monaco FC',
+      implementLeading: true,
+      implementTraling: true,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50, left: 24, right: 24),
+            child: SingleChildScrollView(
+              child: Column(
+                children: listRoom
+                    .map(
+                      (e) => ItemKitsWidget(
+                        itemModel: e,
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: e.image))
+                              .then((_) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Copy thành công!'),
+                              ),
+                            );
+                          });
+                        },
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: _banner == null
+                ? Container()
+                : Container(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: AdWidget(
+                      ad: _banner!,
+                    ),
+                  ),
+          )
+        ],
+      ),
+    );
+  }
+}
